38a2b44b62e138dc914282f3148cfff72a08177c
Your Name <you@example.com>
2025-12-07 10:49:26 +0700

    Update signOutRedirect to use a specific logout URI for improved functionality

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -362,7 +362,7 @@ function App() {
   
   const signOutRedirect = () => {
     const clientId = "5tai0tc43qpu5fq4l8hukmh9q3";
-    const logoutUri = "<logout uri>";
+    const logoutUri = "https://demo.nttdata-cs.com";
     const cognitoDomain = "https://ap-southeast-1gysqnwnf1.auth.ap-southeast-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };

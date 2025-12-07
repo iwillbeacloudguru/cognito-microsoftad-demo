4f0f6d0eec0bd01f71a2df5e19de4f3fa044dd0c
Your Name <you@example.com>
2025-12-07 09:41:20 +0700

    Update docker.sh to include build option in docker-compose command

--- docker.sh
+++ docker.sh
@@ -3,4 +3,4 @@ docker rm sample-app-container
 # docker build -t sample-app .
 # docker run -d -p 3000:3000 --name sample-app-container sample-app
 # docker logs sample-app-container
-docker-compose up -d
+docker-compose up -d --build

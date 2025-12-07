0876cf2343dba3e069477257cd503b9ece9adb08
Your Name <you@example.com>
2025-12-07 09:14:21 +0700

    Refactor PostgreSQL setup: move Dockerfile and init.sql to a dedicated postgres directory, update docker-compose configuration

--- docker.sh
+++ docker.sh
@@ -1,5 +1,6 @@
 docker stop sample-app-container
 docker rm sample-app-container
-docker build -t sample-app .
-docker run -d -p 3000:3000 --name sample-app-container sample-app
-docker logs sample-app-container
+# docker build -t sample-app .
+# docker run -d -p 3000:3000 --name sample-app-container sample-app
+# docker logs sample-app-container
+docker-compose up -d
